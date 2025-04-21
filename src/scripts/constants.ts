06eb0a99d93375884b129bb72123f0f380d21ac9
Sam Fritton <samuel.fritton@target.com>
2025-04-21 14:10:13 -0500

    reducing star count

--- src/scripts/constants.ts
+++ src/scripts/constants.ts
@@ -1,4 +1,4 @@
-export const STAR_COUNT = 400;
+export const STAR_COUNT = 100;
 export const SPEED = 0.0005;
 export const MIN_SPEED = 0.02;
 export const COLORS = ['255,255,255', '204,204,255', '170,170,255', '221,221,255'];
